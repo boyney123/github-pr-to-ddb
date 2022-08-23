44dda1eec103e5e4db63c5ffe494cd6009e19b9b
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2022-08-23 06:33:43 +0100

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,34 @@
-console.log('FROM NODE');
+const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb');
+const client = new DynamoDBClient({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+});
 
-console.log(process.env);
+const main = async () => {
+  try {
+    const command = new PutItemCommand({
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        id: {
+          S: `https://github.com/${process.env.GITHUB_REPOSITORY}/pull/${process.env.PULL_REQUEST_NUMBER}`,
+        },
+        processed: {
+          BOOL: false,
+        },
+        url: {
+          S: `https://serverlessland.com/patterns/cdk-closed-loop-serverless-control`,
+        },
+      },
+    });
+    const response = await client.send(command);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+    console.log('Failed to save PR into DDB');
+  }
+
+};
+
+main();
